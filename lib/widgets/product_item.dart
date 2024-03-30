97f1cdb32be044942a1f4a7dec906b1f420119bd
wahyukaprawi <wahyukaprawi123@gmail.com>
2024-03-30 12:14:36 +0800

    consumer provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,7 +8,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productData = Provider.of<Product>(context);
+    final productData = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -26,12 +26,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: (productData.isFavorite) ? Icon(Icons.favorite) : Icon(Icons.favorite_border_outlined), 
-            color: Colors.amber,
-            onPressed: () {
-              productData.statusfav();
-            },
+          leading: Consumer<Product>(
+            builder: (context, value, child) => IconButton(
+              icon: (productData.isFavorite)
+                  ? Icon(Icons.favorite)
+                  : Icon(Icons.favorite_border_outlined),
+              color: Colors.amber,
+              onPressed: () {
+                productData.statusfav();
+              },
+            ),
           ),
           title: Text(
             productData.getTitle,
